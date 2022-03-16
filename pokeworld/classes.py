d458bda8a807d1afc68344aa6b564d6fba4c5e1b
Chris <chris@hopfen.space>
2022-03-16 01:33:16 +0100

    Removed all GeoJSON-specific classes to replace them with custom classes

--- pokeworld/classes.py
+++ pokeworld/classes.py
@@ -2,81 +2,38 @@ import time
 import uuid as _uuid
 from typing import List, Tuple, Union
 
-from geojson import GeoJSON
-from geojson.geometry import Point, LineString, Polygon
-
 from . import __version__
 
 
-class _GeoJSONBase(GeoJSON):
-    @classmethod
-    def to_instance(cls, ob, default=None, strict=False):
-        raise NotImplemented
+class _Base(dict):
+    def __init__(self, seq=None, **kwargs):
+        super().__init__(seq, **kwargs)
 
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({super().__repr__()})"
 
-class GeoJSONPointProperties:
-    pass
+    __str__ = __repr__
 
+    def __getattr__(self, name):
+        """
+        Permit dictionary items to be retrieved like object attributes
+        """
 
-class GeoJSONPathProperties:
-    pass
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
 
+    def __setattr__(self, name, value):
+        """
+        Permit dictionary items to be set like object attributes
+        """
 
-class GeoJSONAreaProperties:
-    pass
+        self[name] = value
 
+    def __delattr__(self, name):
+        """
+        Permit dictionary items to be deleted like object attributes
+        """
 
-class GeoJSONPoint(_GeoJSONBase):
-    type: str = "Feature"
-    id: Union[None, str, int] = None
-    geometry: Point
-    properties: GeoJSONPointProperties
-
-    def errors(self):
-        return self.geometry.errors()
-
-
-class GeoJSONPath(_GeoJSONBase):
-    type: str = "Feature"
-    id: Union[None, str, int] = None
-    geometry: LineString
-    properties: GeoJSONPathProperties
-
-    def errors(self):
-        return self.geometry.errors()
-
-
-class GeoJSONArea(_GeoJSONBase):
-    type: str = "Feature"
-    id: Union[None, str, int] = None
-    geometry: Polygon
-    properties: GeoJSONAreaProperties
-
-    def errors(self):
-        return self.geometry.errors()
-
-
-class GeoJSONMap(_GeoJSONBase):
-    type: str = "FeatureCollection"
-    uuid: _uuid.UUID
-    bbox: Tuple[float, float, float, float]
-    timestamp: int
-    creator_software: str
-    features: List[Union[GeoJSONPoint, GeoJSONPath, GeoJSONArea]]
-
-    def __init__(
-            self,
-            bbox: Tuple[float, float, float, float],
-            features: List[Union[GeoJSONPoint, GeoJSONPath, GeoJSONArea]],
-            uuid: Union[None, _uuid.UUID, str] = None
-    ):
-        super().__init__()
-        self.bbox = bbox
-        self.features = features
-
-        self.uuid = (uuid if isinstance(uuid, _uuid.UUID) else _uuid.UUID(uuid)) or _uuid.uuid4()
-        self.timestamp = round(time.time())
-        self.creator_software = f"{__package__} {__version__}"
-
-    def errors(self):
-        return [e.errors() for e in self.features]
+        del self[name]
