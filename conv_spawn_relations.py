a0858dc37a4586e55447ac444b88c6ca3e1ec50f
Chris <chris@hopfen.space>
2022-03-25 06:01:05 +0100

    Added the first version of 'convert_spawn_info'

--- conv_spawn_relations.py
+++ conv_spawn_relations.py
@@ -3,18 +3,45 @@
 import os
 import json
 
+MAX_RARITY = 300.0
+RARITY_ROUNDING = 5
+
 SINGLE_STATS_DIR = "."
 SINGLE_STATS_FILES = sorted(list(os.walk(SINGLE_STATS_DIR))[0][2])
 
 ALL_POKEMON_DIR = "."
 ALL_POKEMON_FILES = list(os.walk(ALL_POKEMON_DIR))[0][2]
 
+ALL_KEYS = {
+    "minLevel", "maxLevel", "tags", "spec", "rarityMultipliers", "typeID",
+    "stringLocationTypes", "condition", "anticondition", "heldItems", "rarity"
+}
+
+
+def convert_spawn_info(spawn_info: dict, poke_id: int, name: str, male_chance: float) -> list[dict]:
+    result = []
+    for entry in spawn_info:
+        for k in entry:
+            if k not in ALL_KEYS:
+                print("Unknown key", k, "encountered for", poke_id)
+
+        result.append({
+            "min_level": entry["minLevel"],
+            "max_level": entry["maxLevel"],
+            "male_chance": male_chance,
+            "spawn_area": 1337,  # TODO: select correct ID of SpawnAreaType
+            "probability": round(entry["rarity"] / MAX_RARITY, RARITY_ROUNDING),
+            "conditions": [
+                # TODO: add conditions
+            ]
+        })
+
+    return result
+
+
 def convert_all():
     complete_data = {"spawns": {}}
     pokemon_without_spawn = []
-
-    min_rarity = 10000
-    max_rarity = 0
 
     for filename in SINGLE_STATS_FILES:
         poke_id = int(filename.split(".")[0])
@@ -36,26 +63,14 @@ def convert_all():
 
         with open(spawn_details_file) as fd:
             spawn_data = json.load(fd)
-
-        spawn_info = spawn_data["spawnInfos"]
-        entry = {
-            "name": name,
-            "catch_rate_modifier": data["catchRate"],
-            "male_chance": data["malePercent"] / 100,
-            "min_level": data["spawnLevel"],
-            "max_level": data["spawnLevel"] + data["spawnLevelRange"],
-            "spawn_group": ""
-        }
-        complete_data["spawns"][poke_id] = entry
+        complete_data["spawns"][poke_id] = convert_spawn_info(
+            spawn_data["spawnInfos"], poke_id, name, data["malePercent"] / 100
+        )
 
     complete_data["no_spawns"] = pokemon_without_spawn
-    complete_data["min_rarity"] = min_rarity
-    complete_data["max_rarity"] = max_rarity
 
     with open("conversion_result.json", "w") as fd:
         json.dump(complete_data, fd, indent=2)
-
-    print("min", min_rarity, "max", max_rarity)
 
 
 if __name__ == "__main__":
