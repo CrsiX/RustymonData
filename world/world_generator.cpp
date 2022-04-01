77b322cb88717e34fcc23768dbd04588f3d24fae
Chris <chris@hopfen.space>
2022-04-01 07:52:03 +0200

    Added support for help, removed obsolete code

--- world/world_generator.cpp
+++ world/world_generator.cpp
@@ -54,63 +54,6 @@ public:
 
 class WorldGenerator : public osmium::handler::Handler {
 
-};
-
-
-class MyManager : public osmium::relations::RelationsManager<MyManager, true, true, true> {
-private:
-    int node_count = 0;
-    int way_count = 0;
-    int relation_count = 0;
-    Json::Value root;
-    Json::Value nodes = Json::Value(Json::arrayValue);
-    Json::Value ways = Json::Value(Json::arrayValue);
-    Json::Value relations = Json::Value(Json::arrayValue);
-
-    /// Determine whether a given relation should be ignored in the Poke World
-    bool ignore_relation(const osmium::Relation& relation) {
-        if (relation.tags().has_key("type")) {
-            std::string type = (std::string) relation.tags().get_value_by_key("type");
-            for (std::string s : IGNORED_RELATION_TYPES) {
-                if (type == s) {
-                    return true;
-                }
-            }
-        } else {
-            std::cerr << "Relation " << relation.id() << " has no 'type' tag." << std::endl;
-        }
-        return false;
-    }
-
-    /// Print an OSM object and its tags to stdout
-    void print_object(const osmium::OSMObject& object) {
-        std::cout << "Object " << object.id() << std::endl;
-        for (auto &t : object.tags()) {
-            std::cout << "  " << t.key() << "=" << t.value() << std::endl;
-        }
-    }
-
-public:
-    Json::Value get_root() {
-        this->root["nodes"] = nodes;
-        this->root["ways"] = ways;
-        this->root["relations"] = relations;
-        return this->root;
-    }
-
-    bool new_relation(const osmium::Relation& relation) {
-        return true;
-    }
-
-    bool new_member(const osmium::Relation& relation, const osmium::RelationMember& member, std::size_t) {
-        return true;
-    }
-
-};
-
-
-class WorldGenerator : public osmium::handler::Handler {
-
 public:
 
     Json::Value get_json_data() {
@@ -128,7 +71,7 @@ public:
 };
 
 
-void generate_world(const char *in_file, const char *out_file) {
+void generate_world(const char *in_file, const char *out_file, const osmium::Box bbox) {
     const osmium::io::File input_file{in_file};
 
     osmium::area::Assembler::config_type assembler_config;
@@ -164,11 +107,25 @@ void generate_world(const char *in_file, const char *out_file) {
 }
 
 
+void print_help() {
+    std::cout << "Help is not implemented yet." << std::endl;
+}
+
+
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <InputFile> <OutputFile>" << std::endl;
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "--help") == 0) {
+            print_help();
+            return 0;
+        }
+    }
+
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <InputFile> <OutputFile> <BoundingBox>" << std::endl;
         return 2;
     }
-    generate_world(argv[1], argv[2]);
+
+    osmium::Box bbox;
+    generate_world(argv[1], argv[2], bbox);
     return 0;
 }
