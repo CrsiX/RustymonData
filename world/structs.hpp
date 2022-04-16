ab432ea1202a01c8072e17b703f55c3cd7029d92
Chris <chris@hopfen.space>
2022-04-17 00:00:27 +0200

    Added JSON stream export of Street and Area as well

--- world/structs.hpp
+++ world/structs.hpp
@@ -11,7 +11,7 @@ namespace rustymon {
         struct POI {
             const long oid;
             const int type;
-            std::pair<int, int> pos;
+            std::pair<double, double> pos;
             std::vector<int> spawns;
 
             friend std::ostream& operator << (std::ostream &stream, const POI &poi);
@@ -37,19 +37,63 @@ namespace rustymon {
         struct Street {
             const long oid;
             const int type;
-            std::vector<std::pair<int, int>> waypoints;
+            std::vector<std::pair<double, double>> waypoints;
 
             friend std::ostream& operator << (std::ostream &stream, const Street &street);
         };
 
+        std::ostream& operator << (std::ostream &stream, const Street &street) {
+            stream << "{"
+                   << "\"type\":" << street.type << ","
+                   << "\"oid\":" << street.oid << ","
+                   << "\"points\":[";
+            int i = 0;
+            for (; i + 1 < street.waypoints.size(); i++) {
+                const std::pair<double, double> &point = street.waypoints.at(i);
+                stream << "[" << point.first << "," << point.second << "],";
+            }
+            if (!street.waypoints.empty()) {
+                const std::pair<double, double> &point = street.waypoints.at(i);
+                stream << "[" << point.first << "," << point.second << "]";
+            }
+            stream << "]}";
+            return stream;
+        }
+
         struct Area {
             const long oid;
             const int type;
-            std::vector<std::pair<int, int>> border;
+            std::vector<std::pair<double, double>> border;
             std::vector<int> spawns;
 
             friend std::ostream& operator << (std::ostream &stream, const Area &area);
         };
+
+        std::ostream& operator << (std::ostream &stream, const Area &area) {
+            stream << "{"
+                   << "\"type\":" << area.type << ","
+                   << "\"oid\":" << area.oid << ","
+                   << "\"spawns\":[";
+            int i = 0;
+            for (; i + 1 < area.spawns.size(); i++) {
+                stream << area.spawns.at(i) << ",";
+            }
+            if (!area.spawns.empty()) {
+                stream << area.spawns.at(i);
+            }
+            stream << "],\"points\":[";
+            i = 0;
+            for (; i + 1 < area.border.size(); i++) {
+                const std::pair<double, double> &point = area.border.at(i);
+                stream << "[" << point.first << "," << point.second << "],";
+            }
+            if (!area.border.empty()) {
+                const std::pair<double, double> &point = area.border.at(i);
+                stream << "[" << point.first << "," << point.second << "]";
+            }
+            stream << "]}";
+            return stream;
+        }
 
         struct Tile {
             const osmium::Box bbox;
