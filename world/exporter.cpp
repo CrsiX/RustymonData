13f54efaa45cf44dd81ee1237c707f2869e56b96
Chris <chris@hopfen.space>
2022-04-18 21:04:08 +0200

    Switched to partitioned tile upload with async calls

--- world/exporter.cpp
+++ world/exporter.cpp
@@ -28,6 +28,14 @@ namespace rustymon {
 
         int errors = 0;
         for (auto const& x : world) {
+            /**
+             * An important note to make here is that arguments passed to an asynchronous call are copied.
+             * Under the hood, an asynchronous call through the library’s API is done with std::async.
+             * By default, for memory safety, all arguments are copied (or moved if temporary) because there’s
+             * no syntax level guarantee that the arguments will live beyond the scope of the request.
+             */
+            std::vector<std::future<cpr::Response>> responses{};
+
             for (auto const &y: x.second) {
                 std::stringstream body;
                 body << y.second;
@@ -36,13 +44,19 @@ namespace rustymon {
                 position_header << x.first << "," << y.first;
                 headers.erase("X-Tile-Position");
                 headers.insert({"X-Tile-Position", position_header.str()});
+                responses.emplace_back(cpr::PostAsync(cpr::Url{push_url}, cpr::Body{body.str()}, headers));
+            }
 
-                cpr::Response r = cpr::Post(cpr::Url{push_url}, cpr::Body{body.str()}, headers);
+            int completed_parts = 0;
+            for (std::future<cpr::Response>& future: responses) {
+                cpr::Response r = future.get();
                 if (r.status_code != 200) {
                     errors++;
-                    logger << "Received status code " << r.status_code << " while uploading Tile " << position_header.str() << std::endl;
+                    logger << "Received status code " << r.status_code << " while uploading a tile with x=" << x.first << std::endl;
                 }
+                completed_parts++;
             }
+            logger << "Uploaded " << completed_parts << " parts for x=" << x.first << std::endl;
         }
 
         logger << "Completed uploading of " << counter << " objects with " << errors << " errors." << std::endl;
