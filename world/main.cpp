ecc723b5c02611ce07eb808f15f8999d28d72589
Chris <chris@hopfen.space>
2022-04-20 01:23:55 +0200

    Enabled the config loader in the main file, added plain 'stdout' subcommand

--- world/main.cpp
+++ world/main.cpp
@@ -1,10 +1,10 @@
-#include <chrono>
 #include <string>
 #include <cstring>
 #include <iostream>
 
 #include "structs.hpp"
 #include "constants.hpp"
+#include "config.hpp"
 #include "generator.hpp"
 #include "exporter.hpp"
 
@@ -43,14 +43,18 @@ int main(int argc, char *argv[]) {
             return 2;
         }
 
-        std::cout << "HTTP mode is not implemented yet." << std::endl;
-        rustymon::WorldGenerator generator(config_file);
+        const rustymon::config::Config config = rustymon::config::load_config_from_file(config_file);
+        rustymon::WorldGenerator generator(config);
         rustymon::reader::read_from_file(generator, argv[2]);
         rustymon::export_world_to_http(generator.get_world(), argv[3], auth_info);
         return 0;
     } else if (argc >= 2 && strcmp(argv[1], "dir") == 0) {
         // TODO: add directory support
-        std::cout << "Directory support is not implemented yet." << std::endl;
+        std::cerr << "Directory support is not implemented yet." << std::endl;
+        return 1;
+    } else if (argc >= 2 && strcmp(argv[1], "stdout") == 0) {
+        // TODO: add support for stdout exporting
+        std::cerr << "Stdout support is not implemented yet." << std::endl;
         return 1;
     } else if (argc >= 2 && strcmp(argv[1], "file") == 0) {
         const std::string usage = "Usage: " + std::string(argv[0]) + " file <InputFile> <OutputFile> <BoundingBox> [<ConfigFile>]";
@@ -64,13 +68,13 @@ int main(int argc, char *argv[]) {
 
         osmium::Box bbox = rustymon::helpers::get_bbox(argv[4]);
         std::cout << "Using bounding box " << bbox << "." << std::endl;
-        rustymon::WorldGenerator generator(bbox, config_file);
+        const rustymon::config::Config config = rustymon::config::load_config_from_file(config_file);
+        rustymon::WorldGenerator generator(config, bbox);
         rustymon::reader::read_from_file(generator, argv[2]);
-        rustymon::structs::World &world = generator.get_world();
-        rustymon::export_world_to_file(world, argv[3]);
+        rustymon::export_world_to_file(generator.get_world(), argv[3]);
         return 0;
     } else {
-        std::cerr << "Usage: " << std::string(argv[0]) << " {help,dir,file,http,test} [Options...]" << std::endl;
+        std::cerr << "Usage: " << std::string(argv[0]) << " {help,dir,file,http,stdout,test} [Options...]" << std::endl;
         return 2;
     }
 }
